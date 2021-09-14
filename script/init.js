63955218bd5948a1ac1ceb70cec63bcbcab91fc7
이재홍사원 <jh2000lee@nate.com>
2021-09-14 11:03:02 +0900

    모바일 접속 시 앱에서 보기 유도 모달 추가

--- script/init.js
+++ script/init.js
@@ -5,3 +5,37 @@ export default window.goToPath = (path) => {
     alert(path);
     location.href = path;
 };
+
+// 모바일 판단
+const isMobile = detectMobileDevice(window.navigator.userAgent);
+function detectMobileDevice(agent) {
+    const mobileRegex = [/Android/i, /iPhone/i, /iPad/i, /iPod/i, /BlackBerry/i, /Windows Phone/i];
+
+    return mobileRegex.some((mobile) => agent.match(mobile));
+}
+function goToUrl() {
+    window.open('https://play.google.com/store/apps/details?id=com.insurparts.repair_shop');
+}
+
+$(() => {
+    if (isMobile) {
+        console.log('current device is mobile');
+        // $('body').css('height', 'initial');
+        $('body').append(`
+            <div class="app__guide__modal">
+                <div class="app__guide__wrap">
+                    <img src="/imgs/logo_auto_service_center.png"/>
+                    <p class="app__guide__info">"회원가입하면 첫이용 </br>
+                        10,000원 쿠폰 준대요!"
+                    </p>
+                    <button class="app__guide__button">앱에서 보기</button>
+                    <p class="app__guide__close">괜찮아요. 오늘은 그냥 볼게요.</p>
+            </div>`);
+        $('.app__guide__button').on('click', goToUrl);
+        $('.app__guide__close').on('click', () => {
+            $('.app__guide__modal').remove();
+        });
+    } else {
+        console.log('current device is not mobile');
+    }
+});
