344f1ef1dd66ad2ac02bbd9e641701fbd8dddf36
이재홍사원 <jh2000lee@nate.com>
2021-09-15 16:29:41 +0900

    [MOD]header id 추가, 공지사항 페이지 이동 추가

--- common/js/header.js
+++ common/js/header.js
@@ -1,10 +1,20 @@
 $(() => {
-    $('#logout').on('click', logout);
+    $('.header__item').on('click', goToPage);
 });
 
 // 로그아웃
 function logout(params) {
     if (confirm('로그아웃 하시겠습니까?')) {
         location.href = '../index.html';
+    }
+}
+
+// 메뉴선택, 로고 선택 시 해당 페이지 이동
+function goToPage(event) {
+    console.log(event);
+    if (event.currentTarget.id == 'LOGOUT') {
+        logout();
+    } else if (event.currentTarget.id == 'NOTICE') {
+        location.href = 'noticeList.html';
     }
 }
